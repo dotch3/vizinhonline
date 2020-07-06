8bd30befea81064ccbbbe4223af6fe02e8d6da8e
Jorge Mercado <dotch3@gmail.com>
2020-07-06 20:56:18 -0300

    merge com melhoras de layout

--- resources/views/layouts/users/UserData.blade.php
+++ resources/views/layouts/users/UserData.blade.php
@@ -9,18 +9,89 @@ Asi funciona como exemplo de ter todos os estilos em um solo arquivo CSS -->
 <script src="../js/rendered-js.js"></script>
 <!-- Obtendo o nome do usuario dinamicamente segundo a pagina -->
 
-<section class="user_data">
+<section class="user_data col-12">
 
     <h2>
-        {{ !empty($user->id) ? $user->name: '' }}
+        {{-- {{ !empty($user ?? ''->id) ? $user ?? ''->name: '' }} --}}
+       {{$user->name ?? ' '}}
+       
     </h2>
-    @if(!empty($user->id))
-        <a href="#">
-            <img src="{{!empty($user->image->slug) ? asset('/img/avatar/'.$user->image->slug): '' }} "
-                 onclick="redirectToProfile(this.src)" alt=" perfil" ,
-                 title="perfil" class="perfil"/>
-        </a>
+    {{-- <a href="#">
+        <img src="{{!empty($user ?? ''->id) ? asset('/img/avatar/'.$user ?? ''->image->slug): '' }} "
+             onclick="redirectToProfile(this.src)" alt=" perfil" ,
+             title="perfil" class="perfil"/>
+    </a> --}}
+    <div class="row">
+        <div class="col-8">
+             @if(!empty($user->image->id))
+                {{--                    @elseif(Storage::disk('public')->exists($user->image->name))--}}
 
+                {{-- <img src="{{asset('/storage/avatar/'.$user->image->slug)}}" id="imgProfile" class="profile"
+                     style="width: 180px;height: 170px; "> --}}
+                     <div class="personal-image">
+                        <div class="profile-img img_upload"> 
+                          <label class="label">
+                              <input type="file" multiple accept='image/*' name="image" id="image" style="width: 200px;">
+                            <figure class="personal-figure">
+                              <img id="imgProfile"src="{{asset('/img/avatar/'.$user->image->slug)}}" id="imgProfile" class="personal-avatar" alt="avatar">
+                              <figcaption class="personal-figcaption">
+                                <img src="https://raw.githubusercontent.com/ThiagoLuizNunes/angular-boilerplate/master/src/assets/imgs/camera-white.png" alt="foto">
+                              </figcaption>
+                            </figure>
+                          </label>
+                        </div>
+                      </div>
+
+            @else
+                {{-- <div class=" fundo_img">
+
+                    <h2>Sua Foto</h2>
+                </div> --}}
+                <div class="personal-image">
+                    <div class="profile-img img_upload"> 
+                      <label class="label">
+                          <input type="file" multiple accept='image/*' name="image" id="image" style="width: 200px;">
+                        <figure class="personal-figure">
+                          <img id="imgProfile" src="img/avatar/fundo.png" class="personal-avatar" alt="avatar">
+                          <figcaption class="personal-figcaption">
+                            <img src="https://raw.githubusercontent.com/ThiagoLuizNunes/angular-boilerplate/master/src/assets/imgs/camera-white.png" alt="foto">
+                          </figcaption>
+                        </figure>
+                      </label>
+                    </div>
+                  </div>
+            @endif
+        </div>
+    </div>  
+    <div class="dadosUser">
+    <div class="col-12"> 
+    <ul class="fa-ul">
+        {{-- <li><span class="fa-li"><i class="fas fa-user" style="font-size:20px ; color: #000000"></i>
+            </span>{{ !empty($user ?? ''->id) ? $user ?? ''->name." ".$user ?? ''->lastname: '' }}</li> --}}
+        {{-- <li> <i class="fas fa-user" style="font-size:20px, color: #000000"></i> {{$user->name ?? ' '}} {{$user->lastname ?? ' '}}</li>
+        {{-- <li><span class="fa-li"><i class="fas fa-envelope"
+                                   style="font-size:20px ; color: #000000"></i></span>{{ !empty($user ?? ''->id) ? $user ?? ''->email: '' }}
+        </li> --}}
+        {{--<li> <i class="fas fa-envelope" style="font-size:20px, color: #000000"></i> {{$user->email ?? ' '}}</li>--}}
+
+        {{-- <li><span class="fa-li"><i class="fas fa-building"
+                                   style="font-size:20px ; color: #000000"></i></span>{{ !empty($user ?? ''->id) ? $user ?? ''->location->building." ".$user ?? ''->location->apartment_number: '' }}
+        </li> --}}
+        {{--<li> <i class="fas fa-building" style="font-size:20px, color: #000000"></i> {{$locations->building ?? ' '}}</li>}}
+        {{-- <li><span class="fa-li"><i class="fas fa-check-square" style="font-size:20px ; color: #000000"></i></span>
+            Mensagem
+        </li> --}}
+        <!-- verificar se os itens abaixo serão necessarios, pois invite é muito complicado a programacao e contagem de emprestimo será comprometida se fizer fora da plataforma -->
+        <!-- <li><span class="fa-li"><i class="fas fa-users" style="font-size:20px ; color: #000000"></i></span>3 Vizinhos -->
+        <!-- </li> -->
+        <!-- <li><span class="fa-li"><i class="fas fa-people-carry" style="font-size:20px ; color: #000000"></i></span>3 -->
+        <!-- Emprestimos</li> -->
+        {{-- <li><i class="fas fa-thumbs-up" style="font-size:20px ; color: #000000"></i>Recomende-me</li>
+        <li><i class="fas fa-heart" style="font-size:20px ; color: #000000"></i>Recomendações </li>
+    </ul>
+    </ul> --}}
+
+    <div class="container">
         <ul class="fa-ul">
             <li><span class="fa-li"><i class="fas fa-user" style="font-size:20px ; color: #000000"></i>
             </span>{{ !empty($user->id) ? $user->name." ".$user->lastname: '' }}</li>
@@ -47,24 +118,7 @@ Asi funciona como exemplo de ter todos os estilos em um solo arquivo CSS -->
                 Recomendações
             </li>
         </ul>
-
-        <div class="container">
-            <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-cog" style="font-size:20px ; color: #000000"></i></span>
-                    <a style="color:inherit" href="{{!empty($user->id)? route('users.profile',$user->id) :''}}">
-                        Configuração de conta</a>
-                </li>
-                <li><a style="color:inherit" href="/"><span class="fa-li"><i class="fas fas fa-sign-out-alt"
-                                                                             style="font-size:20px ; color: #000000"></i></span>
-                        Sair</a></li>
-
-            </ul>
-        </div>
-    @else
-        <div class=" fundo_img">
-            <h2>Usuario invalido</h2>
-        </div>
-    @endif
-
-
+    </div>
+    </div>
+</div>
 </section>
