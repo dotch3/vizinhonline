b04ccb9044c4f6aaa8d13878ee27bfe3f93919f2
felpe522 <felipe.albiero@hotmail.com>
2020-07-07 15:13:16 -0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/estilo.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
