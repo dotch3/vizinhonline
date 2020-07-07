8dd17cbcdd78dac3d3627163a11a8c5b0c29f0b2
felpe522 <felipe.albiero@hotmail.com>
2020-07-07 13:45:47 -0300

    middlewareFeedbacks

--- app/Http/Controllers/FeedbacksController.php
+++ app/Http/Controllers/FeedbacksController.php
@@ -7,6 +7,10 @@ use App\Feedbacks;
 
 class FeedbacksController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
