591469915a21fb4eec6aff8b0aa0af4add7a9b89
felpe522 <felipe.albiero@hotmail.com>
2020-07-15 17:38:41 -0300

    teste

---
+++ resources/views/layouts/main/EmConstrucao.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="{{ asset('/css/estilo.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/estilo_header.css') }}" rel="stylesheet">
+    <title>Home - Vizinho online</title>
+</head>
+
+<body>
+<main>
+    <div class="row">
+        <header class="header">
+            <nav>
+            </nav>
+        </header>
+    <div class="row">
+
+        <div class="fundoTexto">
+            <h2>EM CONSTRUÇÃO</h2>
+            <div class="col-2">
+            </div>
+            <div class="texto col-10">
+                <p>
+                    Em breve lançaremos a funcionalidade!
+                </p>
+            </div>
+        </div>
+
+    </div>
+    @include('.layouts/main/Footer')
+
+        
+
+</main>
+<!-- Including the footer -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
+</body>
+
+
+</html>
